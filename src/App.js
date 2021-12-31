ef3bd6ab3edca979bd28e964ed0f0d6d19e005b0
Kwon <anstjdrnjs3@gmail.com>
2021-12-31 14:00:35 +0900

    R: practice useEffect

--- src/App.js
+++ src/App.js
@@ -4,15 +4,32 @@ import styles from './App.module.css'
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   const onClick = () => setCounter(prev => prev + 1);
+  const onChange = e => setKeyword(e.target.value);
   console.log('i run all the time');
 
   const iRunOnlyOnce = () => {
-    console.log('i run only once');
+    console.log('CALL The API...');
   }
   useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5) {
+      console.log('SEARCH FOR', keyword);
+    }
+  }, [keyword])
+  useEffect(() => {
+    console.log('I run when keyword and counter change');
+  }, [keyword, counter]);
   return (
     <div>
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type='text' 
+        placeholder='Search hear...'
+        />
       <h1 className={styles.title}>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
